c3a0a9b08afc32a53c51be8a4e9f885a6935db61
Daniil <daniilsavin3910@gmail.com>
2024-04-13 18:32:15 +0300

    add loading site

--- main.py
+++ main.py
@@ -1,45 +1,70 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import pytesseract
-from PIL import Image, ImageDraw
+import os, sys, re
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
 
 
-def screenshot(url, title):
-    web = 'https://' + url
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--hide-scrollbars")
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get(web)
-    print(driver.get_screenshot_as_file(f'{title}.png'))
-    driver.quit()
+def savePage(url, pagepath):
+    def savenRename(soup, pagefolder, session, url, tag, inner):
+        if not os.path.exists(pagefolder):  # create only once
+            os.mkdir(pagefolder)
+        for res in soup.findAll(tag):  # images, css, etc..
+            if tag == 'title':
+                res.string = '{title}'
+            elif tag == 'style':
+                try:
+                    text = res.string.strip()
+                    if 'url' in text:
+                        print(text)
+                        s = re.search(r"url\([^)]*\)", text)
+                        urls = text[s.start() + 4: s.end() - 1]
+                        fileurl = urljoin(url, urls)
+                        filename = urls.split('/')[-1]
+                        filepath = os.path.join(pagefolder, filename)
+                        print(filepath)
+                        print(os.path.isfile(filepath))
+                        res.string = res.string[:s.start() + 5] + './' + str(
+                            os.path.join(os.path.basename(pagefolder), filename)).replace('\\', '/') + res.string[
+                                                                                                       s.end():]
+                        if not os.path.isfile(filepath):  # was not downloaded
+                            with open(filepath, 'wb') as file:
+                                filebin = session.get(fileurl)
+                                file.write(filebin.content)
+                        print(urls)
+                except Exception as exc:
+                    print(exc, file=sys.stderr)
+
+            elif res.has_attr(inner):  # check inner tag (file object) MUST exists
+                try:
+                    filename, ext = os.path.splitext(os.path.basename(res[inner]))  # get name and extension
+                    if '?' in ext:
+                        ext = ext[:ext.find('?')]
+                    filename = re.sub('\W+', '', filename) + ext  # clean special chars from name
+                    fileurl = urljoin(url, res.get(inner))
+                    filepath = os.path.join(pagefolder, filename)
+                    # rename html ref so can move html and folder of files anywhere
+                    res[inner] = os.path.join(os.path.basename(pagefolder), filename)
+                    if not os.path.isfile(filepath):  # was not downloaded
+                        with open(filepath, 'wb') as file:
+                            filebin = session.get(fileurl)
+                            file.write(filebin.content)
+
+                except Exception as exc:
+                    print(exc, file=sys.stderr)
+
+    path, _ = os.path.splitext(pagepath)
+    pagefolder = path + '_files'
+    session = requests.Session()
+    # ... whatever other requests config you need here
+    response = session.get(url)
+    soup = BeautifulSoup(response.content.decode('utf-8'), "html.parser")
+    tags_inner = {'img': 'src', 'link': 'href', 'script': 'src', 'style': '', 'title': ''}  # tag&inner tags to grab
+    for tag, inner in tags_inner.items():  # saves resource files and rename refs
+        savenRename(soup, pagefolder, session, url, tag, inner)
+    with open(path + '.html', 'wb') as file:  # saves modified html doc
+        file.write(soup.prettify('utf-8'))
+    with open(path + '.html', 'a') as file:
+        file.write('<style>a {pointer-events: none;} button {pointer-events: none;}</style>\n ')
 
 
-def detect_title(title):
-    global data, image_copy
-    image = Image.open(f"{title}.png")
-    image_copy = image.copy()
-    target_word = title
-    data = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT)
-    word_occurences = [i for i, word in enumerate(data["text"]) if word.lower() == target_word]
-
-    for occ in word_occurences:
-        blur_words(occ, title)
-
-
-def blur_words(occ, title):
-    w = data["width"][occ]
-    h = data["height"][occ]
-    l = data["left"][occ]
-    t = data["top"][occ]
-    p1 = (l, t)
-    p2 = (l + w, t)
-    p3 = (l + w, t + h)
-    p4 = (l, t + h)
-    draw = ImageDraw.Draw(image_copy)
-    draw.rectangle((*p1, *p3))
-    image_copy.save(f"{title}_copy.png")
-
-
-detect_title('github')
+savePage('https://github.com/', 'github')
