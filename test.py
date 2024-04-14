c7577c650b17ccf635dc4cb0eaf1500090052bbc
IvanZasukhin <i89041056398@gmail.com>
2024-04-14 22:14:34 +0300

    add function ban

--- test.py
+++
@@ -1,5 +0,0 @@
-from requests import post
-
-print(post(f'http://localhost:8080/api/users', json={'login': "None2",
-                                                     'description': "NONO",
-                                                     'hashed_password': "1234"}).json()["message"])
